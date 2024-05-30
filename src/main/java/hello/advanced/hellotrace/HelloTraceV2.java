df93c9b3cf991213ebc875ff1f434eedddc914bf
sedna99 <ljsmmm1228@gmail.com>
2024-05-30 23:30:10 +0900

    섹션1: 로그 추적기 V2 - 파라미터로 동기화 개발

--- src/main/java/hello/advanced/hellotrace/HelloTraceV2.java
+++ src/main/java/hello/advanced/hellotrace/HelloTraceV2.java
@@ -15,14 +15,16 @@ public class HelloTraceV2 {
     public TraceStatus begin(String message) {
         TraceId traceId = new TraceId();
         Long startTimeMs = System.currentTimeMillis();
-        log.info("traceId={}, message={}", traceId, message);
+        log.info("[" + traceId.getId() + "] " + addSpace(START_PREFIX,
+                traceId.getLevel()) + message);
         return new TraceStatus(traceId, startTimeMs, message);
     }
 
     public TraceStatus beginSync(TraceId beforeTraceId, String message) {
         TraceId nextId = beforeTraceId.createNextId();
         Long startTimeMs = System.currentTimeMillis();
-        log.info("traceId={}, message={}", nextId, message);
+        log.info("[" + nextId.getId() + "] " + addSpace(START_PREFIX,
+                nextId.getLevel()) + message);
         return new TraceStatus(nextId, startTimeMs, message);
     }
 
