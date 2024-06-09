f2169682982b41574f08025136ac24523b4436ba
sedna99 <ljsmmm1228@gmail.com>
2024-06-09 23:09:10 +0900

    섹션3: 템플릿 메서드 패턴 - 예제3

--- src/test/java/hello/advanced/template/code/AbstractTemplate.java
+++ src/test/java/hello/advanced/template/code/AbstractTemplate.java
@@ -1,11 +1,14 @@
 package hello.advanced.template.code;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public abstract class AbstractTemplate {
     public void execute() {
         long begin = System.currentTimeMillis();
         call();
         long end = System.currentTimeMillis();
-        System.out.println("resultTime: " + (end - begin));
+        log.info("resultTime: {}", (end - begin));
     }
 
     protected abstract void call();
