93b6bb01e0b8265a42a5f4ec98d13ee7e7610b42
sedna99 <ljsmmm1228@gmail.com>
2024-06-22 23:22:10 +0900

    섹션4: 프록시 패턴 - 예제 코드1

---
+++ proxy-start/src/test/java/hello/proxy/ProxyPatternTest.java
@@ -0,0 +1,18 @@
+package hello.proxy;
+
+import hello.proxy.code.ProxyPatternClient;
+import hello.proxy.code.RealSubject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ProxyPatternTest {
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
---
+++ proxy-start/src/test/java/hello/proxy/code/ProxyPatternClient.java
@@ -0,0 +1,13 @@
+package hello.proxy.code;
+
+public class ProxyPatternClient {
+    private Subject subject;
+
+    public ProxyPatternClient(Subject subject) {
+        this.subject = subject;
+    }
+
+    public String execute() {
+        return subject.operation();
+    }
+}
