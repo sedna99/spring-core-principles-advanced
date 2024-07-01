e50e58f392a25fe55dedd0fdcd6ced85bb9fdac9
sedna99 <ljsmmm1228@gmail.com>
2024-07-01 23:01:10 +0900

    섹션5: 리플렉션

---
+++ proxy-start/src/test/java/hello/pureproxy/proxy/jdkdynamic/ReflectionTest.java
@@ -0,0 +1,64 @@
+package hello.pureproxy.proxy.jdkdynamic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ReflectionTest {
+    @Test
+    void reflection0() {
+        Hello hello = new Hello();
+        log.info("start");
+        String result1 = hello.callA();
+        log.info("result1: {}", result1);
+
+        log.info("start");
+        String result2 = hello.callB();
+        log.info("result2: {}", result2);
+    }
+
+    @Test
+    void reflection1() throws Exception {
+        Class classHello = Class.forName("hello.pureproxy.proxy.jdkdynamic.ReflectionTest$Hello");
+        Hello target = new Hello();
+        Method callA = classHello.getMethod("callA");
+        Object result1 = callA.invoke(target);
+        log.info("result1: {}", result1);
+
+        Method callB = classHello.getMethod("callB");
+        Object result2 = callB.invoke(target);
+        log.info("result2: {}", result2);
+    }
+
+    @Test
+    void reflection2() throws Exception {
+        Class classHello = Class.forName("hello.pureproxy.proxy.jdkdynamic.ReflectionTest$Hello");
+        Hello target = new Hello();
+        Method callA = classHello.getMethod("callA");
+        dynamicCall(callA, target);
+
+        Method callB = classHello.getMethod("callB");
+        dynamicCall(callB, target);
+    }
+    private void dynamicCall(Method method, Object target) throws Exception {
+        log.info("start");
+        Object result = method.invoke(target);
+        log.info("result: {}", result);
+    }
+
+    @Slf4j
+    static class Hello {
+        public String callA() {
+            log.info("callA");
+            return "A";
+        }
+
+        public String callB() {
+            log.info("callB");
+            return "B";
+        }
+    }
+}
